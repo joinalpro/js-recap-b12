2c67ea6800295b21afd06a9250616689c9ce5ebc
Joinal Abden <joinalzx@gmail.com>
2025-07-29 18:15:05 +0600

    js intro

---
+++ intro-to-js/type.js
@@ -0,0 +1,13 @@
+// string
+var myAddress = "Chariani Prodhania Bari!"
+console.log(myAddress);
+
+// boolean
+var isStudent = true;
+console.log(typeof(isStudent));
+
+// number
+var price = 22;
+console.log(typeof price);
+
+// how to write long variable name
