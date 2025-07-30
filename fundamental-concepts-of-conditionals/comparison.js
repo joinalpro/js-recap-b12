4c46cf5bef2730101939a476a1236dc68b26ab14
Joinal Abden <joinalzx@gmail.com>
2025-07-30 23:26:08 +0600

    js condition

---
+++ fundamental-concepts-of-conditionals/comparison.js
@@ -0,0 +1,15 @@
+// console.log( 5 < 10);
+// console.log( 5 == 10);
+// console.log( 15 >= 10);
+// console.log( 5 >= 10);
+
+/**
+ * ==
+ * === it's check value and type
+ * != not equals tos
+ */
+
+console.log( 10 == '10');
+console.log(10 === '10');
+
+console.log(10 !== '10' );
---
+++ fundamental-concepts-of-conditionals/js-conditional.js
@@ -0,0 +1,9 @@
+// Comparison
+// >, <, ==, >=, <=, !=, &&, ||
+
+// let age = 17;
+// let withAdult = true;
+
+// if (age >= 15){
+//     console.log("you can enter in Mela.");
+// }
