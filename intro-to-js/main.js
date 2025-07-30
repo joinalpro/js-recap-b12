a5bb67eba983567fe4c71c00f37d4731eb104fcf
Joinal Abden <joinalzx@gmail.com>
2025-07-30 16:22:24 +0600

    intro to js done

---
+++ intro-to-js/main.js
@@ -0,0 +1,26 @@
+// let orangePrice = 26;
+
+// // console.log(typeof orangePrice);
+// console.log(orangePrice);
+
+// // let potatoPrice = '20';
+// // console.log(typeof potatoPrice);
+
+// let potatoPrice = 20;
+
+// console.log(orangePrice + potatoPrice);
+
+// // use .toFixed (), will show number of decimal
+
+// let firstNum = 0.1;
+// let secondNum = 0.2;
+// let total = firstNum + secondNum;
+// // parseInt and parseFloat
+// console.log(total.toFixed(2));
+
+// let price = 10
+// // price +=2
+// price *=2
+// console.log(price);
+
+// // 
