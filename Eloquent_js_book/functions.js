986daa2fd4037c4b30f84e5c4ccf48d83355380f
Joinal Abden <joinalzx@gmail.com>
2025-07-31 12:51:24 +0600

    if else conditions

---
+++ Eloquent_js_book/functions.js
@@ -0,0 +1,12 @@
+// let yourPasscode = prompt("Enter your passcode: ");
+// console.log(yourPasscode);
+
+// Control Flow
+
+// let theNumber = Number(prompt("Pick a number: "));
+// console.log("Your number is the square root of " + theNumber * theNumber);
+
+let theNumber = Number(prompt("Pick a number: "));
+if (!Number.isNaN(theNumber)){
+    console.log("Your number is the square root of " + theNumber * theNumber);
+}
---
+++ Eloquent_js_book/variable.js
@@ -0,0 +1,6 @@
+// let luigisDebt = 140;
+// luigisDebt = luigisDebt - 35;
+// console.log(luigisDebt);
+
+let one = 1, two = 2;
+console.log(one + two);
