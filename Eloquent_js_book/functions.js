0efd3460f64f25ab6da6ed5a081a5fe64f487219
Joinal Abden <joinalzx@gmail.com>
2025-08-02 22:48:38 +0600

    while, do loops

--- Eloquent_js_book/functions.js
+++ Eloquent_js_book/functions.js
@@ -6,7 +6,11 @@
 // let theNumber = Number(prompt("Pick a number: "));
 // console.log("Your number is the square root of " + theNumber * theNumber);
 
-let theNumber = Number(prompt("Pick a number: "));
+/**
+ * let theNumber = Number(prompt("Pick a number: "));
 if (!Number.isNaN(theNumber)){
     console.log("Your number is the square root of " + theNumber * theNumber);
 }
+* 
+*/
+
---
+++ Eloquent_js_book/while-loop.js
@@ -0,0 +1,23 @@
+// let number = 0;
+// while ( number <= 12){
+//     // console.log(number);
+//     if(number % 2 == 0){
+//         console.log(number);
+//     }
+//     number++;
+// }
+
+// let result = 1;
+// let counter = 0;
+
+// while (counter < 10 ){
+//     result = result * 2;
+//     counter++
+// }
+// console.log(result);
+
+let yourName ;
+do {
+    yourName = prompt("Who are you?");
+}while (!yourName);
+console.log(yourName);
