9ddeaf809abd11d0379e11991dbfdbb341d15cd9
Joinal Abden <joinalzx@gmail.com>
2025-08-06 16:57:48 +0600

    fundamental

---
+++ fundamental-concepts-of-conditionals/break.js
@@ -0,0 +1,16 @@
+// for (let i = 0; i < 150; i++) {
+//     console.log(i);
+//     if(i >= 5) {
+//         break;
+//     }
+// }
+// console.log('Life after break');
+
+let n = 54;
+while(n > 25){
+    if(n <50){
+        break;
+    }
+    console.log(n);
+    n--;
+}
--- fundamental-concepts-of-conditionals/decrementail-while.js
+++ fundamental-concepts-of-conditionals/decrementail-while.js
@@ -1,9 +1,9 @@
-let num = 20;
-let sum = 0;
-while (num >= 0){
-    console.log(num);
-    sum = sum + num
-    // console.log(sum);
-    num--;
-}
-console.log(sum);
+// let num = 20;
+// let sum = 0;
+// while (num >= 0){
+//     console.log(num);
+//     sum = sum + num
+//     // console.log(sum);
+//     num--;
+// }
+// console.log(sum);
