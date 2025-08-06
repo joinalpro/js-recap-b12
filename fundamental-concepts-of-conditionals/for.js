8feead41e942f2edabc5980ac25e2fb93af5d893
Joinal Abden <joinalzx@gmail.com>
2025-08-06 12:51:08 +0600

    while and for loop

---
+++ fundamental-concepts-of-conditionals/for.js
@@ -0,0 +1,14 @@
+
+// for (let num = 0; num < 5; num++){
+//     console.log(num);
+// }
+
+// even
+for(let i = 0; i <=20; i= i+2){
+    // console.log(i);
+}
+
+// odd
+for(let i = 1; i<=20; i= i+2){
+    console.log(i);
+}
