951eca7fc1aff10bdb3574f6f82a8f491f155ce6
Joinal Abden <joinalzx@gmail.com>
2025-08-09 13:02:11 +0600

    array, object, function

---
+++ functions-fundamental/function.js
@@ -0,0 +1,27 @@
+// basic function
+
+/**
+ * function hello (){
+    console.log("this is a functoion");
+}
+hello();
+ */
+
+// function moneybag(){
+//     let money = 4500; // Local Variable
+//     console.log("I have " + money + " Taka");
+// }
+
+// moneybag();
+// console.log(money);
+
+
+function jogkoro(in1, in2){
+    let sum = in1 + in2;
+    console.log("Sum is = " + sum);
+    return sum;
+}
+
+// jogkoro(10, 20);
+let total = jogkoro(10, 20);
+console.log(total);
